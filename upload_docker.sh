3ba33df66c60b3a7853bd11b666c08a781d87e91
Hay Marn <haymarn03@gmail.com>
2022-10-30 01:39:06 +0800

    Added upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,12 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=haymarnhm/ml-microservice:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
-# Push image to a docker repository
+docker login
+docker push haymarnhm/ml-microservice:v1.0.0 
